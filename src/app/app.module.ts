f865256dd8bfb3b5f45a92830b78943db7d524f6
dhythm <Yuta.Okada.20@gmail.com>
2020-06-12 22:45:23 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,40 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultingDetailComponent } from './consulting-detail/consulting-detail.component';
 import { ConsultingListComponent } from './consulting-list/consulting-list.component';
 import { FabComponent } from './fab/fab.component';
-import { ConsultingDetailComponent } from './consulting-detail/consulting-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, ConsultingListComponent, FabComponent, ConsultingDetailComponent],
+  declarations: [
+    AppComponent,
+    ConsultingListComponent,
+    FabComponent,
+    ConsultingDetailComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
     FlexLayoutModule,
   ],
   providers: [],
--- src/app/consulting-detail/consulting-detail.component.ts
+++ src/app/consulting-detail/consulting-detail.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-consulting-detail',
   templateUrl: './consulting-detail.component.html',
-  styleUrls: ['./consulting-detail.component.css']
+  styleUrls: ['./consulting-detail.component.css'],
 })
 export class ConsultingDetailComponent implements OnInit {
+  consultingForm: FormGroup;
+  contentControl = new FormControl();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.consultingForm = this.formBuilder.group({
+      content: this.contentControl,
+      checkbox: false,
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(data): void {
+    this.consultingForm.reset();
+    console.warn('submit: ', data);
+  }
 }
