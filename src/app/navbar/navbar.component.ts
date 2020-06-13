ea5553c2c445e3ec9f8a6735721de9e1efc74d75
dhythm <Yuta.Okada.20@gmail.com>
2020-06-13 11:02:53 +0900

    use dynamic page title

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +11,36 @@ export class NavbarComponent implements OnInit {
   title = 'angular-sample';
   @Output() event = new EventEmitter<String>();
 
-  constructor() {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd)
+        // map(() => this.activatedRoute)
+      )
+      .subscribe((event: NavigationEnd) => {
+        switch (event.url) {
+          case '/':
+          case '/list':
+            this.title = 'Consultation Categories';
+            break;
+          case '/consultations':
+            this.title = 'Previous Consultations';
+            break;
+          case '/consultant':
+            this.title = 'Consultant Menu';
+            break;
+          default:
+            this.title = 'angular-sample';
+        }
+      });
+    // this.router.events
+    //   .filter((event) => event instanceof NavigationEnd)
+    //   .subscribe((event) => {
+    //     console.log({ event });
+    //   });
+  }
 
   onToggleSidenav(): void {
     this.event.emit('clicked');
